e6df491220e4f7d9ea2ae23f057cc8c4e8cb6e8a
Maxim_shmidt <history1266@mail.ru>
2022-12-12 13:47:18 +0700

    задание 38

---
+++ task38/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+Console.WriteLine("Введите размер массива");
+int a = Convert.ToInt32(Console.ReadLine());
+double[] array = new double[a];
+double result = 0;
+double min = 100;
+double max = 0;
+for(int i=0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(1,100);
+    if(array[i] > max)
+    {
+        max = array[i];
+    }
+    else if(array[i] < min){
+        min = array[i];
+    }
+}
+result = max - min;
+Console.WriteLine($"[{String.Join(", ", array)}]");
+Console.WriteLine(result);
