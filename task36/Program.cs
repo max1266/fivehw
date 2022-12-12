83a9d11ea37ce1e96d64c528193375a8d1455b92
Maxim_shmidt <history1266@mail.ru>
2022-12-12 13:24:04 +0700

    задание 36

---
+++ task36/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.WriteLine("Введите размер массива");
+int a = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[a];
+int summ = 0;
+for(int i=0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(-99,100);
+    if( i % 2 !=0)
+    {
+        summ = summ + array[i];
+    }
+   
+}
+Console.WriteLine($"[{String.Join(",", array)}]");
+Console.WriteLine(summ);
