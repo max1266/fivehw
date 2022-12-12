54fd4b85ca6464d92db0ffabc2c03529d74f67b6
Maxim_shmidt <history1266@mail.ru>
2022-12-12 13:04:39 +0700

    Задание 34

---
+++ task34/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.WriteLine("Введите размер массива");
+int a = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[a];
+int count = 0;
+for(int i=0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(100,200);
+    if(array[i] % 2 == 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine($"[{String.Join(", ", array)}]");
+Console.WriteLine($"Количество четных элементов массива: {count}");
